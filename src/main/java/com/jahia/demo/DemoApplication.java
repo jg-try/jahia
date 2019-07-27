ea80bf05a0af639c0c256fdffd0c9c464ada0f05
jgt <jg.try@everteam.com>
2019-07-27 13:33:28 +0200

    add User entity and controller

--- src/main/java/com/jahia/demo/DemoApplication.java
+++ src/main/java/com/jahia/demo/DemoApplication.java
@@ -1,7 +1,14 @@
 package com.jahia.demo;
 
+import java.util.stream.Stream;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.jahia.demo.entities.User;
+import com.jahia.demo.repositories.UserRepository;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +17,14 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+    @Bean
+    CommandLineRunner init(UserRepository userRepository) {
+        return args -> {
+            Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
+                User user = new User(name, name.toLowerCase() + "@domain.com");
+                userRepository.save(user);
+            });
+            userRepository.findAll().forEach(System.out::println);
+        };
+    }
 }
